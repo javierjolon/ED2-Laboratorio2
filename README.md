9b20bfa36b59261827077de0f72964205de39fbf
Mario Jose Roldan <mariojudoka@gmail.com>
2020-09-22 23:16:45 -0600

    create the scheme
    the structure of lab 2
    I need the disk B* :(

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
-# ED2-Laboratorio2
-Estructura de datos 2 - Laboratorio 2 
+# TreeBDisk
---
+++ SerieII/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SerieII
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
